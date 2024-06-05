23dd1970731295430ada0dc8a07810ca285729a5
Zhanserik <220401@astanait.edu.kz>
2024-06-05 18:09:24 +0500

    fix маленьких багов

--- TruckBot/config.py
+++ TruckBot/config.py
@@ -1,5 +1,8 @@
 import os
 from dotenv import load_dotenv
+
+load_dotenv()
+
 TORTOISE_ORM = {
     "connections": {"default": "sqlite://db.sqlite3"},
     "apps": {
--- TruckBot/main.py
+++ TruckBot/main.py
@@ -1,12 +1,12 @@
-import asyncio
-from aiogram import Bot, Dispatcher, types
+from aiogram import Bot
 from fastapi import FastAPI
 from tortoise.contrib.fastapi import register_tortoise
-from fastapi.middleware.cors import CORSMiddleware
 from schemas import UserMessage
 from models.models import Truck
+from config import TOKEN
+
 app = FastAPI()
-bot = Bot(token="6385726296:AAGpeQ1aLGpzJj0SrNtDxZf4_2i-FC8h-z8")
+bot = Bot(token=TOKEN)
 
 
 register_tortoise(
@@ -21,4 +21,8 @@ register_tortoise(
 @app.post("/send_message/")
 async def send_message(user_message: UserMessage):
     truck = await Truck.filter(id=user_message.truck_id).first()
-    await bot.send_message(user_message.user_id, str(truck))
+    try:
+        await bot.send_message(user_message.user_id, str(truck))
+        return {"status": "Message sent"}
+    except Exception as e:
+        return {"status": f"Error: {e}"}
